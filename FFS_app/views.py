958326e0a8613a81a1a67fe2fee81cad75f64836
Tomas Smolskas <tomas.smolskas@gmail.com>
2022-10-19 21:30:19 +0300

    some first steps

--- FFS_app/views.py
+++ FFS_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
+from django.contrib.auth import login
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse('home'))
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/sign_up.html', context={'form': form})
+
+
+@login_required
+def user_profile(request):
+    return render(request, 'registration/user_profile.html')
